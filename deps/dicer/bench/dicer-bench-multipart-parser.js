0a04eedfc6232398245c2abeb5dd60210fab73d6
uzlopak <aras.abbasi@googlemail.com>
2021-11-28 16:30:03 +0100

    fix linting error

--- deps/dicer/bench/dicer-bench-multipart-parser.js
+++ deps/dicer/bench/dicer-bench-multipart-parser.js
@@ -1,5 +1,18 @@
 const Dicer = require('../lib/Dicer')
 
+function createMultipartBuffer(boundary, size) {
+  const head =
+    '--' + boundary + '\r\n'
+    + 'content-disposition: form-data; name="field1"\r\n'
+    + '\r\n'
+    , tail = '\r\n--' + boundary + '--\r\n'
+    , buffer = Buffer.allocUnsafe(size);
+
+  buffer.write(head, 0, 'ascii');
+  buffer.write(tail, buffer.length - tail.length, 'ascii');
+  return buffer;
+}
+
 for (let i = 0, il = 10; i < il; i++) {
   const boundary = '-----------------------------168072824752491622650073',
     d = new Dicer({ boundary: boundary }),
@@ -36,23 +49,10 @@ for (let i = 0, il = 10; i < il; i++) {
     callbacks.end++;
   });
 
-  var start = +new Date(),
-    nparsed = d.write(buffer),
-    duration = +new Date - start,
-    mbPerSec = (mb / (duration / 1000)).toFixed(2);
+  const start = +new Date();
+  d.write(buffer);
+  const duration = +new Date - start;
+  const mbPerSec = (mb / (duration / 1000)).toFixed(2);
 
   console.log(mbPerSec + ' mb/sec');
-
-  function createMultipartBuffer(boundary, size) {
-    const head =
-      '--' + boundary + '\r\n'
-      + 'content-disposition: form-data; name="field1"\r\n'
-      + '\r\n'
-      , tail = '\r\n--' + boundary + '--\r\n'
-      , buffer = Buffer.allocUnsafe(size);
-
-    buffer.write(head, 0, 'ascii');
-    buffer.write(tail, buffer.length - tail.length, 'ascii');
-    return buffer;
-  }
 }
