b2b67e2cc2bfda37c257510651a862a19ebcc7b9
uzlopak <aras.abbasi@googlemail.com>
2021-12-01 22:43:25 +0100

    modify unit test

--- test/types-multipart.spec.js
+++ test/types-multipart.spec.js
@@ -246,7 +246,32 @@ describe('types-multipart', () => {
       expected: [
         ['field', 'upload_file_0', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', false, false, '7bit', 'application/json']
       ],
-      what: 'Blob uploads should be handled as fields.'
+      what: 'Blob uploads should be handled as fields if isPartAFile is provided.'
+    },
+    {
+      config: {
+        isPartAFile: (fieldName) => (fieldName !== 'upload_file_0')
+      },
+      source: [
+        ['-----------------------------paZqsnEHRufoShdX6fh0lUhXBP4k',
+          'Content-Disposition: form-data; name="upload_file_0"; filename="blob"',
+          'Content-Type: application/json',
+          '',
+          'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+          '-----------------------------paZqsnEHRufoShdX6fh0lUhXBP4k',
+          'Content-Disposition: form-data; name="file"; filename*=utf-8\'\'n%C3%A4me.txt',
+          'Content-Type: application/octet-stream',
+          '',
+          'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+          '-----------------------------paZqsnEHRufoShdX6fh0lUhXBP4k--'
+        ].join('\r\n')
+      ],
+      boundary: '---------------------------paZqsnEHRufoShdX6fh0lUhXBP4k',
+      expected: [
+        ['field', 'upload_file_0', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', false, false, '7bit', 'application/json'],
+        ['file', 'file', 26, 0, 'näme.txt', '7bit', 'application/octet-stream']
+      ],
+      what: 'Blob uploads should be handled as fields if isPartAFile is provided. Other fields should be files.'
     },
     {
       source: [
