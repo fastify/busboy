2bf1a014ab36b1f6a9bcaecd8c901e9af091e32e
Giovanni Bucci <giovanni.bucci@nearform.com>
2024-04-19 12:53:55 +0200

    added the plan for the subtest too in the dicer-write.test.js

--- test/dicer-write.test.js
+++ test/dicer-write.test.js
@@ -7,6 +7,7 @@ test('dicer _write method', t => {
   t.plan(1)
 
   t.test('calls the callback cb() when headerFirst is set and all the data have been written', t => {
+    t.plan(1)
     const dicer = new Dicer({ headerFirst: true })
 
     dicer._write(Buffer.from('Content-Type: text/plain'), null, () => {
