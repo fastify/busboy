c0eff10e5525377954ca408347365640df41de71
Giovanni Bucci <giovanni.bucci@nearform.com>
2024-04-19 09:14:15 +0200

    added a couple more tests and eslint fixes

--- test/dicer-write.test.js
+++ test/dicer-write.test.js
@@ -4,17 +4,17 @@ const { test } = require('tap')
 const { Dicer } = require('../lib/main')
 
 test('dicer _write method', t => {
-    t.plan(1);
+  t.plan(1)
 
-    t.test('the PartStream instance is created only once if the _write method is called more than once', t => {
-        const dicer = new Dicer({headerFirst: true});
+  t.test('the PartStream instance is created only once if the _write method is called more than once', t => {
+    const dicer = new Dicer({ headerFirst: true })
 
-        dicer._write(Buffer.from('Content-Type: text/plain'), null, () => {
-            dicer._write(Buffer.from('Content-Type: text/plain'), null, () => {
-                t.pass('write method called');
-            });
-        });
+    dicer._write(Buffer.from('Content-Type: text/plain'), null, () => {
+      dicer._write(Buffer.from('Content-Type: text/plain'), null, () => {
+        t.pass('write method called')
+      })
+    })
 
-        t.end();
-    });
-});
+    t.end()
+  })
+})
