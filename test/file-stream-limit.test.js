1aa3a6123213c0da4ef0a78d8e47d6e912b271e8
William Ferreira da Silva <williamfdasilva@gmail.com>
2025-06-02 08:09:52 -0300

    test: migrate BusboyFileStream emits limit from tap to node:test

--- test/file-stream-limit.test.js
+++ test/file-stream-limit.test.js
@@ -2,36 +2,38 @@
 
 const { PassThrough } = require('stream')
 const Busboy = require('../').default
-const { test } = require('tap')
+const { test } = require('node:test')
+const assert = require('node:assert')
 
-test('BusboyFileStream emits limit', t => {
-  t.plan(1)
+test('BusboyFileStream emits limit', async (t) => {
+  await new Promise((resolve, reject) => {
+    const bigPayload = Buffer.alloc(20, 'a')
+    const boundary = 'foo'
+    const req = new PassThrough()
+    req.headers = {
+      'content-type': `multipart/form-data; boundary=${boundary}`
+    }
 
-  const bigPayload = Buffer.alloc(20, 'a')
-  const boundary = 'foo'
-  const req = new PassThrough()
-  req.headers = {
-    'content-type': `multipart/form-data; boundary=${boundary}`
-  }
-
-  const bb = new Busboy({
-    headers: req.headers,
-    limits: { fileSize: 10 }
-  })
-
-  bb.on('file', (_fieldname, stream) => {
-    stream.on('limit', () => {
-      t.pass('limit event emitted')
+    const bb = new Busboy({
+      headers: req.headers,
+      limits: { fileSize: 10 }
     })
-    stream.resume()
+
+    bb.on('file', (_fieldname, stream) => {
+      stream.on('limit', () => {
+        assert.ok(true, 'limit event emitted')
+        resolve()
+      })
+      stream.resume()
+    })
+
+    req.pipe(bb)
+
+    const delimiter = `--${boundary}`
+    req.write(`${delimiter}\r\n`)
+    req.write('Content-Disposition: form-data; name="file"; filename="a.txt"\r\n\r\n')
+    req.write(bigPayload)
+    req.write(`\r\n${delimiter}--\r\n`)
+    req.end()
   })
-
-  req.pipe(bb)
-
-  const delimiter = `--${boundary}`
-  req.write(`${delimiter}\r\n`)
-  req.write('Content-Disposition: form-data; name="file"; filename="a.txt"\r\n\r\n')
-  req.write(bigPayload)
-  req.write(`\r\n${delimiter}--\r\n`)
-  req.end()
 })
