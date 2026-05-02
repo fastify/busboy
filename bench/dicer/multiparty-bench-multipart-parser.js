8ac87754e40f91245c250d03438e7e72b5a45e4c
Antonio Tripodi <Tony133@users.noreply.github.com>
2026-05-02 22:31:50 +0200

    chore: update eslint config and others updated (#206)

--- bench/dicer/multiparty-bench-multipart-parser.js
+++ bench/dicer/multiparty-bench-multipart-parser.js
@@ -1,78 +1,79 @@
 'use strict'
 
-var assert = require('node:assert'),
-    Form = require('multiparty').Form,
-    boundary = '-----------------------------168072824752491622650073',
-    mb = 100,
-    buffer = createMultipartBuffer(boundary, mb * 1024 * 1024),
-    callbacks =
-      { partBegin: -1,
+const assert = require('node:assert')
+const Form = require('multiparty').Form
+const boundary = '-----------------------------168072824752491622650073'
+const mb = 100
+const buffer = createMultipartBuffer(boundary, mb * 1024 * 1024)
+const callbacks =
+      {
+        partBegin: -1,
         partEnd: -1,
         headerField: -1,
         headerValue: -1,
         partData: -1,
         end: -1,
-      };
+      }
 
-var form = new Form({ boundary: boundary });
+const form = new Form({ boundary })
 
-hijack('onParseHeaderField', function() {
-  callbacks.headerField++;
-});
+hijack('onParseHeaderField', function () {
+  callbacks.headerField++
+})
 
-hijack('onParseHeaderValue', function() {
-  callbacks.headerValue++;
-});
+hijack('onParseHeaderValue', function () {
+  callbacks.headerValue++
+})
 
-hijack('onParsePartBegin', function() {
-  callbacks.partBegin++;
-});
+hijack('onParsePartBegin', function () {
+  callbacks.partBegin++
+})
 
-hijack('onParsePartData', function() {
-  callbacks.partData++;
-});
+hijack('onParsePartData', function () {
+  callbacks.partData++
+})
 
-hijack('onParsePartEnd', function() {
-  callbacks.partEnd++;
-});
+hijack('onParsePartEnd', function () {
+  callbacks.partEnd++
+})
 
-form.on('finish', function() {
-  callbacks.end++;
-});
+form.on('finish', function () {
+  callbacks.end++
+})
 
-var start = new Date();
-form.write(buffer, function(err) {
-  var duration = new Date() - start;
-  assert.ifError(err);
-  var mbPerSec = (mb / (duration / 1000)).toFixed(2);
-  console.log(mbPerSec+' mb/sec');
-});
+const start = new Date()
+form.write(buffer, function (err) {
+  const duration = new Date() - start
+  assert.ifError(err)
+  const mbPerSec = (mb / (duration / 1000)).toFixed(2)
+  console.log(mbPerSec + ' mb/sec')
+})
 
-//assert.equal(nparsed, buffer.length);
+// assert.equal(nparsed, buffer.length);
 
-function createMultipartBuffer(boundary, size) {
-  var head =
-        '--'+boundary+'\r\n'
-      + 'content-disposition: form-data; name="field1"\r\n'
-      + '\r\n'
-    , tail = '\r\n--'+boundary+'--\r\n'
-    , buffer = Buffer.allocUnsafe(size);
+function createMultipartBuffer (boundary, size) {
+  const head =
+        '--' + boundary + '\r\n' +
+      'content-disposition: form-data; name="field1"\r\n' +
+      '\r\n'
+  const tail = '\r\n--' + boundary + '--\r\n'
+  const buffer = Buffer.allocUnsafe(size)
 
-  buffer.write(head, 'ascii', 0);
-  buffer.write(tail, 'ascii', buffer.length - tail.length);
-  return buffer;
+  buffer.write(head, 'ascii', 0)
+  buffer.write(tail, 'ascii', buffer.length - tail.length)
+  return buffer
 }
 
-process.on('exit', function() {
-  /*for (var k in callbacks) {
+process.on('exit', function () {
+  /* for (var k in callbacks) {
     assert.equal(0, callbacks[k], k+' count off by '+callbacks[k]);
-  }*/
-});
+  } */
+})
 
-function hijack(name, fn) {
-  var oldFn = form[name];
-  form[name] = function() {
-    fn();
-    return oldFn.apply(this, arguments);
-  };
+function hijack (name, fn) {
+  const oldFn = form[name]
+  form[name] = function () {
+    fn()
+    return oldFn.apply(this, arguments)
+  }
 }
--- test-types/main.test-d.ts
+++ test-types/main.test-d.ts
@@ -1,241 +1,236 @@
-import BusboyDefault, { BusboyConstructor, BusboyConfig, BusboyHeaders, Busboy, BusboyEvents, BusboyFileStream } from '..';
-import {expectError, expectType} from "tsd";
-import BusboyESM from "..";
+/* eslint-disable import-x/no-duplicates */
+/* eslint-disable no-new */
+import BusboyDefault, { Busboy, BusboyFileStream } from '..'
+import { expectError, expectType } from 'tsd'
+import BusboyESM from '..'
 
-// test type exports
-type Constructor = BusboyConstructor;
-type Config = BusboyConfig;
-type Headers = BusboyHeaders;
-type Events = BusboyEvents;
-type BB = Busboy;
+expectType<InstanceType<typeof Busboy>>(new BusboyESM({ headers: { 'content-type': 'foo' } }))
+expectType<InstanceType<typeof Busboy>>(new Busboy({ headers: { 'content-type': 'foo' } }))
 
-expectType<Busboy>(new BusboyESM({ headers: { 'content-type': 'foo' } }));
-expectType<Busboy>(new Busboy({ headers: { 'content-type': 'foo' } }));
-
-expectError(new BusboyDefault({}));
-const busboy = BusboyDefault({ headers: { 'content-type': 'foo' } }); // $ExpectType Busboy
-new BusboyDefault({ headers: { 'content-type': 'foo' } }); // $ExpectType Busboy
-new BusboyDefault({ headers: { 'content-type': 'foo' }, highWaterMark: 1000 }); // $ExpectType Busboy
-new BusboyDefault({ headers: { 'content-type': 'foo' }, fileHwm: 1000 }); // $ExpectType Busboy
-new BusboyDefault({ headers: { 'content-type': 'foo' }, defCharset: 'utf8' }); // $ExpectType Busboy
-new BusboyDefault({ headers: { 'content-type': 'foo' }, preservePath: true }); // $ExpectType Busboy
-new BusboyDefault({ headers: { 'content-type': 'foo' }, limits: { fieldNameSize: 200 } }); // $ExpectType Busboy
-new BusboyDefault({ headers: { 'content-type': 'foo' }, limits: { fieldSize: 200 } }); // $ExpectType Busboy
-new BusboyDefault({ headers: { 'content-type': 'foo' }, limits: { fields: 200 } }); // $ExpectType Busboy
-new BusboyDefault({ headers: { 'content-type': 'foo' }, limits: { fileSize: 200 } }); // $ExpectType Busboy
-new BusboyDefault({ headers: { 'content-type': 'foo' }, limits: { files: 200 } }); // $ExpectType Busboy
-new BusboyDefault({ headers: { 'content-type': 'foo' }, limits: { parts: 200 } }); // $ExpectType Busboy
-new BusboyDefault({ headers: { 'content-type': 'foo' }, limits: { headerPairs: 200 } }); // $ExpectType Busboy
-new BusboyDefault({ headers: { 'content-type': 'foo' }, limits: { headerSize: 200 } }); // $ExpectType Busboy
-new BusboyDefault({ headers: { 'content-type': 'foo' }, isPartAFile: (fieldName, contentType, fileName) => fieldName === 'my-special-field' || fileName !== 'not-so-special.txt' }); // $ExpectType Busboy
-new BusboyDefault({ headers: { 'content-type': 'foo' }, isPartAFile: (fieldName, contentType, fileName) => fileName !== undefined }); // $ExpectType Busboy
+expectError(new BusboyDefault({}))
+const busboy = BusboyDefault({ headers: { 'content-type': 'foo' } }) // $ExpectType Busboy
+new BusboyDefault({ headers: { 'content-type': 'foo' } }) // $ExpectType Busboy
+new BusboyDefault({ headers: { 'content-type': 'foo' }, highWaterMark: 1000 }) // $ExpectType Busboy
+new BusboyDefault({ headers: { 'content-type': 'foo' }, fileHwm: 1000 }) // $ExpectType Busboy
+new BusboyDefault({ headers: { 'content-type': 'foo' }, defCharset: 'utf8' }) // $ExpectType Busboy
+new BusboyDefault({ headers: { 'content-type': 'foo' }, preservePath: true }) // $ExpectType Busboy
+new BusboyDefault({ headers: { 'content-type': 'foo' }, limits: { fieldNameSize: 200 } }) // $ExpectType Busboy
+new BusboyDefault({ headers: { 'content-type': 'foo' }, limits: { fieldSize: 200 } }) // $ExpectType Busboy
+new BusboyDefault({ headers: { 'content-type': 'foo' }, limits: { fields: 200 } }) // $ExpectType Busboy
+new BusboyDefault({ headers: { 'content-type': 'foo' }, limits: { fileSize: 200 } }) // $ExpectType Busboy
+new BusboyDefault({ headers: { 'content-type': 'foo' }, limits: { files: 200 } }) // $ExpectType Busboy
+new BusboyDefault({ headers: { 'content-type': 'foo' }, limits: { parts: 200 } }) // $ExpectType Busboy
+new BusboyDefault({ headers: { 'content-type': 'foo' }, limits: { headerPairs: 200 } }) // $ExpectType Busboy
+new BusboyDefault({ headers: { 'content-type': 'foo' }, limits: { headerSize: 200 } }) // $ExpectType Busboy
+new BusboyDefault({ headers: { 'content-type': 'foo' }, isPartAFile: (fieldName, contentType, fileName) => fieldName === 'my-special-field' || fileName !== 'not-so-special.txt' }) // $ExpectType Busboy
+new BusboyDefault({ headers: { 'content-type': 'foo' }, isPartAFile: (fieldName, contentType, fileName) => fileName !== undefined }) // $ExpectType Busboy
 
 busboy.addListener('file', (fieldname, file, filename, encoding, mimetype) => {
-    expectType<string> (fieldname)
-    expectType<BusboyFileStream>(file);
-    expectType<string>(filename);
-    expectType<string>(encoding);
-    expectType<string>(mimetype);
-});
+  expectType<string>(fieldname)
+  expectType<BusboyFileStream>(file)
+  expectType<string>(filename)
+  expectType<string>(encoding)
+  expectType<string>(mimetype)
+})
 busboy.addListener('field', (fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) => {
-    expectType<string> (fieldname);
-    expectType<string> (val);
-    expectType<boolean> (fieldnameTruncated);
-    expectType<boolean> (valTruncated);
-    expectType<string> (encoding);
-    expectType<string> (mimetype);
-});
-busboy.addListener('partsLimit', () => {});
-busboy.addListener('filesLimit', () => {});
-busboy.addListener('fieldsLimit', () => {});
+  expectType<string>(fieldname)
+  expectType<string>(val)
+  expectType<boolean>(fieldnameTruncated)
+  expectType<boolean>(valTruncated)
+  expectType<string>(encoding)
+  expectType<string>(mimetype)
+})
+busboy.addListener('partsLimit', () => {})
+busboy.addListener('filesLimit', () => {})
+busboy.addListener('fieldsLimit', () => {})
 busboy.addListener('error', e => {
-    expectType<unknown> (e);
-});
-busboy.addListener('finish', () => {});
+  expectType<unknown>(e)
+})
+busboy.addListener('finish', () => {})
 // test fallback
 busboy.on('foo', foo => {
-    expectType<any> (foo);
-});
+  expectType<any>(foo)
+})
 busboy.on(Symbol('foo'), foo => {
-    expectType<any>(foo);
-});
+  expectType<any>(foo)
+})
 
 busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
-    expectType<string> (fieldname);
-    expectType<BusboyFileStream> (file);
-    expectType<string> (filename);
-    expectType<string> (encoding);
-    expectType<string> (mimetype);
-});
+  expectType<string>(fieldname)
+  expectType<BusboyFileStream>(file)
+  expectType<string>(filename)
+  expectType<string>(encoding)
+  expectType<string>(mimetype)
+})
 busboy.on('field', (fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) => {
-    expectType<string> (fieldname);
-    expectType<string> (val);
-    expectType<boolean> (fieldnameTruncated);
-    expectType<boolean> (valTruncated);
-    expectType<string> (encoding);
-    expectType<string> (mimetype);
-});
-busboy.on('partsLimit', () => {});
-busboy.on('filesLimit', () => {});
-busboy.on('fieldsLimit', () => {});
+  expectType<string>(fieldname)
+  expectType<string>(val)
+  expectType<boolean>(fieldnameTruncated)
+  expectType<boolean>(valTruncated)
+  expectType<string>(encoding)
+  expectType<string>(mimetype)
+})
+busboy.on('partsLimit', () => {})
+busboy.on('filesLimit', () => {})
+busboy.on('fieldsLimit', () => {})
 busboy.on('error', e => {
-    expectType<unknown> (e);
-});
-busboy.on('finish', () => {});
+  expectType<unknown>(e)
+})
+busboy.on('finish', () => {})
 // test fallback
 busboy.on('foo', foo => {
-    expectType<any> (foo);
-});
+  expectType<any>(foo)
+})
 busboy.on(Symbol('foo'), foo => {
-    expectType<any> (foo);
-});
+  expectType<any>(foo)
+})
 
 busboy.once('file', (fieldname, file, filename, encoding, mimetype) => {
-    expectType<string> (fieldname);
-    expectType<BusboyFileStream> (file);
-    expectType<string> (filename);
-    expectType<string> (encoding);
-    expectType<string> (mimetype);
-});
+  expectType<string>(fieldname)
+  expectType<BusboyFileStream>(file)
+  expectType<string>(filename)
+  expectType<string>(encoding)
+  expectType<string>(mimetype)
+})
 busboy.once('field', (fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) => {
-    expectType<string> (fieldname);
-    expectType<string> (val);
-    expectType<boolean> (fieldnameTruncated);
-    expectType<boolean> (valTruncated);
-    expectType<string> (encoding);
-    expectType<string> (mimetype);
-});
-busboy.once('partsLimit', () => {});
-busboy.once('filesLimit', () => {});
-busboy.once('fieldsLimit', () => {});
+  expectType<string>(fieldname)
+  expectType<string>(val)
+  expectType<boolean>(fieldnameTruncated)
+  expectType<boolean>(valTruncated)
+  expectType<string>(encoding)
+  expectType<string>(mimetype)
+})
+busboy.once('partsLimit', () => {})
+busboy.once('filesLimit', () => {})
+busboy.once('fieldsLimit', () => {})
 busboy.once('error', e => {
-    expectType<unknown> (e);
-});
-busboy.once('finish', () => {});
+  expectType<unknown>(e)
+})
+busboy.once('finish', () => {})
 // test fallback
 busboy.once('foo', foo => {
-    expectType<any> (foo);
-});
+  expectType<any>(foo)
+})
 busboy.once(Symbol('foo'), foo => {
-    expectType<any> (foo);
-});
+  expectType<any>(foo)
+})
 
 busboy.removeListener('file', (fieldname, file, filename, encoding, mimetype) => {
-    expectType<string> (fieldname);
-    expectType<BusboyFileStream> (file);
-    expectType<string> (filename);
-    expectType<string> (encoding);
-    expectType<string> (mimetype);
-});
+  expectType<string>(fieldname)
+  expectType<BusboyFileStream>(file)
+  expectType<string>(filename)
+  expectType<string>(encoding)
+  expectType<string>(mimetype)
+})
 busboy.removeListener('field', (fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) => {
-    expectType<string> (fieldname);
-    expectType<string> (val);
-    expectType<boolean> (fieldnameTruncated);
-    expectType<boolean> (valTruncated);
-    expectType<string> (encoding);
-    expectType<string> (mimetype);
-});
-busboy.removeListener('partsLimit', () => {});
-busboy.removeListener('filesLimit', () => {});
-busboy.removeListener('fieldsLimit', () => {});
+  expectType<string>(fieldname)
+  expectType<string>(val)
+  expectType<boolean>(fieldnameTruncated)
+  expectType<boolean>(valTruncated)
+  expectType<string>(encoding)
+  expectType<string>(mimetype)
+})
+busboy.removeListener('partsLimit', () => {})
+busboy.removeListener('filesLimit', () => {})
+busboy.removeListener('fieldsLimit', () => {})
 busboy.removeListener('error', e => {
-    expectType<unknown> (e);
-});
-busboy.removeListener('finish', () => {});
+  expectType<unknown>(e)
+})
+busboy.removeListener('finish', () => {})
 // test fallback
 busboy.removeListener('foo', foo => {
-    expectType<any> (foo);
-});
+  expectType<any>(foo)
+})
 busboy.removeListener(Symbol('foo'), foo => {
-    expectType<any> (foo);
-});
+  expectType<any>(foo)
+})
 
 busboy.off('file', (fieldname, file, filename, encoding, mimetype) => {
-    expectType<string> (fieldname);
-    expectType<BusboyFileStream> (file);
-    expectType<string> (filename);
-    expectType<string> (encoding);
-    expectType<string> (mimetype);
-});
+  expectType<string>(fieldname)
+  expectType<BusboyFileStream>(file)
+  expectType<string>(filename)
+  expectType<string>(encoding)
+  expectType<string>(mimetype)
+})
 busboy.off('field', (fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) => {
-    expectType<string> (fieldname);
-    expectType<string> (val);
-    expectType<boolean> (fieldnameTruncated);
-    expectType<boolean> (valTruncated);
-    expectType<string> (encoding);
-    expectType<string> (mimetype);
-});
-busboy.off('partsLimit', () => {});
-busboy.off('filesLimit', () => {});
-busboy.off('fieldsLimit', () => {});
+  expectType<string>(fieldname)
+  expectType<string>(val)
+  expectType<boolean>(fieldnameTruncated)
+  expectType<boolean>(valTruncated)
+  expectType<string>(encoding)
+  expectType<string>(mimetype)
+})
+busboy.off('partsLimit', () => {})
+busboy.off('filesLimit', () => {})
+busboy.off('fieldsLimit', () => {})
 busboy.off('error', e => {
-    expectType<unknown> (e);
-});
-busboy.off('finish', () => {});
+  expectType<unknown>(e)
+})
+busboy.off('finish', () => {})
 // test fallback
 busboy.off('foo', foo => {
-    expectType<any> (foo);
-});
+  expectType<any>(foo)
+})
 busboy.off(Symbol('foo'), foo => {
-    expectType<any> (foo);
-});
+  expectType<any>(foo)
+})
 
 busboy.prependListener('file', (fieldname, file, filename, encoding, mimetype) => {
-    expectType<string> (fieldname);
-    expectType<BusboyFileStream> (file);
-    expectType<string> (filename);
-    expectType<string> (encoding);
-    expectType<string> (mimetype);
-});
+  expectType<string>(fieldname)
+  expectType<BusboyFileStream>(file)
+  expectType<string>(filename)
+  expectType<string>(encoding)
+  expectType<string>(mimetype)
+})
 busboy.prependListener('field', (fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) => {
-    expectType<string> (fieldname);
-    expectType<string> (val);
-    expectType<boolean> (fieldnameTruncated);
-    expectType<boolean> (valTruncated);
-    expectType<string> (encoding);
-    expectType<string> (mimetype);
-});
-busboy.prependListener('partsLimit', () => {});
-busboy.prependListener('filesLimit', () => {});
-busboy.prependListener('fieldsLimit', () => {});
+  expectType<string>(fieldname)
+  expectType<string>(val)
+  expectType<boolean>(fieldnameTruncated)
+  expectType<boolean>(valTruncated)
+  expectType<string>(encoding)
+  expectType<string>(mimetype)
+})
+busboy.prependListener('partsLimit', () => {})
+busboy.prependListener('filesLimit', () => {})
+busboy.prependListener('fieldsLimit', () => {})
 busboy.prependListener('error', e => {
-    expectType<unknown> (e);
-});
-busboy.prependListener('finish', () => {});
+  expectType<unknown>(e)
+})
+busboy.prependListener('finish', () => {})
 // test fallback
 busboy.prependListener('foo', foo => {
-    expectType<any> (foo);
-});
+  expectType<any>(foo)
+})
 busboy.prependListener(Symbol('foo'), foo => {
-    expectType<any> (foo);
-});
+  expectType<any>(foo)
+})
 
 busboy.prependOnceListener('file', (fieldname, file, filename, encoding, mimetype) => {
-    expectType<string> (fieldname);
-    expectType<BusboyFileStream> (file);
-    expectType<string> (filename);
-    expectType<string> (encoding);
-    expectType<string> (mimetype);
-});
+  expectType<string>(fieldname)
+  expectType<BusboyFileStream>(file)
+  expectType<string>(filename)
+  expectType<string>(encoding)
+  expectType<string>(mimetype)
+})
 busboy.prependOnceListener('field', (fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) => {
-    expectType<string> (fieldname);
-    expectType<string> (val);
-    expectType<boolean> (fieldnameTruncated);
-    expectType<boolean> (valTruncated);
-    expectType<string> (encoding);
-    expectType<string> (mimetype);
-});
-busboy.prependOnceListener('partsLimit', () => {});
-busboy.prependOnceListener('filesLimit', () => {});
-busboy.prependOnceListener('fieldsLimit', () => {});
+  expectType<string>(fieldname)
+  expectType<string>(val)
+  expectType<boolean>(fieldnameTruncated)
+  expectType<boolean>(valTruncated)
+  expectType<string>(encoding)
+  expectType<string>(mimetype)
+})
+busboy.prependOnceListener('partsLimit', () => {})
+busboy.prependOnceListener('filesLimit', () => {})
+busboy.prependOnceListener('fieldsLimit', () => {})
 busboy.prependOnceListener('error', e => {
-    expectType<unknown> (e);
-});
-busboy.prependOnceListener('finish', () => {});
+  expectType<unknown>(e)
+})
+busboy.prependOnceListener('finish', () => {})
 // test fallback
 busboy.prependOnceListener('foo', foo => {
-    expectType<any> (foo);
-});
+  expectType<any>(foo)
+})
 busboy.prependOnceListener(Symbol('foo'), foo => {
-    expectType<any> (foo);
-});
+  expectType<any>(foo)
+})
